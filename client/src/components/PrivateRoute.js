f265b4cf828eb0cbb56d97571ab56f3be889768b
Lucas Gao <lucas2005gao@gmail.com>
2021-04-18 22:20:14 +1200

    added source for api call based authentication

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -6,6 +6,8 @@ import axios from "axios";
 /**
  * This component masks the specified route to users that are not authenticated through
  * the firebase application and redirects them to the login screen
+ * The API call based authentication process is adapted from:
+ * https://gist.github.com/EduVencovsky/f8f6c275f42f7352571c92a59309e31d
  * @param component The page view that the user is trying to access
  * @param rest The rest of the component props
  */
